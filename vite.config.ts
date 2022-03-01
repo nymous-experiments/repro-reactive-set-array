a57e902d8cf242018b4dd21bf93d126dd5ee7976
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2022-03-02 00:14:16 +0100

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/repro-reactive-set-array/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
